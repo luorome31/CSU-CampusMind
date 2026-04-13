48f7c656c0d6dbd1eade1ec15987158db23722db
luorome31 <2743057396@qq.com>
2026-04-13 18:29:51 +0800

    chore(mobile): 添加 spacing 导出

--- mobile/src/styles/index.ts
+++ mobile/src/styles/index.ts
@@ -1,2 +1,3 @@
 export { colors, type ColorKey } from './tokens/colors';
 export { typography, textStyles, type Typography } from './tokens/typography';
+export { spacing, layout } from './tokens/spacing';
